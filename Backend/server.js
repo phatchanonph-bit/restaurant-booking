17e40cfea1066475e252c366b270cfbd1b131190
phatchanonph-bit <phatchanon.ph@ku.th>
2026-03-15 23:58:48 +0700

    ddd

--- Backend/server.js
+++ Backend/server.js
@@ -8,40 +8,40 @@ app.use(cors())
 app.use(express.json())
 
 const db = mysql.createConnection({
-    host:"db",
-    user:"root",
-    password:"1234",
-    database:"restaurant"
+  host: "db",
+  user: "root",
+  password: "1234",
+  database: "restaurant"
 })
 
-db.connect(err=>{
-    if(err){
-        console.log("DB Error",err)
-    }else{
-        console.log("MySQL Connected")
+db.connect(err => {
+  if (err) {
+    console.log("DB Error", err)
+  } else {
+    console.log("MySQL Connected")
+  }
+})
+
+app.post("/book", (req, res) => {
+
+  const { name, phone, people, date, time } = req.body
+
+  const sql = `
+    INSERT INTO bookings
+    (name,phone,people,date,time)
+    VALUES (?,?,?,?,?)
+  `
+
+  db.query(sql, [name, phone, people, date, time], (err, result) => {
+    if (err) {
+      res.json({ error: err })
+    } else {
+      res.json({ message: "Booking success" })
     }
-})
-
-app.post("/book",(req,res)=>{
-
-    const {name,phone,people,date,time} = req.body
-
-    const sql = `
-        INSERT INTO bookings
-        (name,phone,people,date,time)
-        VALUES (?,?,?,?,?)
-    `
-
-    db.query(sql,[name,phone,people,date,time],(err,result)=>{
-        if(err){
-            res.json({error:err})
-        }else{
-            res.json({message:"Booking success"})
-        }
-    })
+  })
 
 })
 
-app.listen(3000,()=>{
-    console.log("Server running on port 3000")
+app.listen(3000, () => {
+  console.log("Server running on port 3000")
 })
